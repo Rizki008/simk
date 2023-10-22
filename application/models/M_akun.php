6d53bbd52b45686970ad95c2d6ac93fb3dea7c7a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-10-22 23:10:14 +0700

    new crud anggota

--- application/models/M_akun.php
+++ application/models/M_akun.php
@@ -36,6 +36,42 @@ class M_akun extends CI_Model
 		$this->db->where('id_akun', $data['id_akun']);
 		$this->db->delete('akun', $data);
 	}
+
+	// ANGGOTA
+	// List all your items
+	public function anggota()
+	{
+		$this->db->select('*');
+		$this->db->from('anggota');
+		$this->db->order_by('id_anggota', 'desc');
+		return $this->db->get()->result();
+	}
+
+	// Add a new item
+	public function add_anggota($data)
+	{
+		$this->db->insert('anggota', $data);
+	}
+
+	//Update one item
+	public function update_anggota($data)
+	{
+		$this->db->where('id_anggota', $data['id_anggota']);
+		$this->db->update(
+			'anggota',
+			$data
+		);
+	}
+
+	//Delete one item
+	public function delete_anggota($data)
+	{
+		$this->db->where('id_anggota', $data['id_anggota']);
+		$this->db->delete(
+			'anggota',
+			$data
+		);
+	}
 }
 
 /* End of file M_akun.php */
