719ea7b725d3cb0349dc0a02a20a28439f72ed9a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-10-24 13:05:43 +0700

    menambahkan function laporan

--- application/models/M_laporan.php
+++ application/models/M_laporan.php
@@ -42,6 +42,70 @@ class M_laporan extends CI_Model
 	{
 		return $this->db->query("SELECT *,SUM(saldo) as total FROM `transaksi` WHERE jenis_saldo='kredit' AND MONTHNAME(tgl_transaksi)='" . $bulan . "' AND YEAR(tgl_transaksi)='" . $tahun . "'")->result();
 	}
+
+	// LABAR RUGI 
+	public function bulan()
+	{
+		return $this->db->query("SELECT *,YEAR(tgl_transaksi) AS tahun from transaksi GROUP BY tahun")->result();
+	}
+	public function akuns_laba($tahun)
+	{
+		return $this->db->query("SELECT * FROM `transaksi` LEFT JOIN akun ON akun.no_reff=transaksi.no_reff WHERE YEAR(tgl_transaksi)='" . $tahun . "' GROUP BY transaksi.no_reff")->result();
+	}
+	public function jurnaldetail_laba($no_reff, $tahun)
+	{
+		return $this->db->query("SELECT transaksi.id_transaksi,transaksi.tgl_transaksi,akun.nama_reff,transaksi.no_reff,transaksi.jenis_saldo,transaksi.saldo,transaksi.tgl_input, YEAR(tgl_transaksi) AS tahun FROM `transaksi` LEFT JOIN `akun` ON `akun`.`no_reff` = `transaksi`.`no_reff` WHERE transaksi.no_reff='" . $no_reff . "' AND YEAR(tgl_transaksi)='" . $tahun . "'")->result();
+	}
+	public function jurnaldetailsaldo_laba($no_reff, $tahun)
+	{
+		return $this->db->query("SELECT transaksi.jenis_saldo,transaksi.saldo, YEAR(tgl_transaksi) AS tahun FROM `transaksi` LEFT JOIN `akun` ON `akun`.`no_reff` = `transaksi`.`no_reff` WHERE transaksi.no_reff='" . $no_reff . "' AND  YEAR(tgl_transaksi)='" . $tahun . "'")->result();
+	}
+	public function jurnaldetailsatupendapatan_laba($tahun)
+	{
+		return $this->db->query("SELECT *, YEAR(tgl_transaksi) AS tahun FROM `transaksi` LEFT JOIN `akun` ON `akun`.`no_reff` = `transaksi`.`no_reff` WHERE YEAR(tgl_transaksi)='" . $tahun . "' AND status='transaksi' AND jenis_saldo='kredit' AND transaksi.no_reff !='4-111' AND transaksi.no_reff !='4-112' AND transaksi.no_reff !='4-113'")->result();
+	}
+	public function jurnaldetailsatubeban_laba($tahun)
+	{
+		return $this->db->query("SELECT *, YEAR(tgl_transaksi) AS tahun FROM `transaksi` LEFT JOIN `akun` ON `akun`.`no_reff` = `transaksi`.`no_reff` WHERE YEAR(tgl_transaksi)='" . $tahun . "' AND status='transaksi' AND jenis_saldo='debit' AND transaksi.no_reff !='4-111' AND transaksi.no_reff !='4-112' AND transaksi.no_reff !='4-113'")->result();
+	}
+	public function jurnaldetailsatukeuangan_laba($tahun)
+	{
+		return $this->db->query("SELECT *, YEAR(tgl_transaksi) AS tahun FROM `transaksi` LEFT JOIN `akun` ON `akun`.`no_reff` = `transaksi`.`no_reff` WHERE YEAR(tgl_transaksi)='" . $tahun . "' AND status='transaksi' AND transaksi.no_reff !='4-111' AND transaksi.no_reff !='4-112' AND transaksi.no_reff !='4-113'")->result();
+	}
+	public function hasildebit_laba($tahun)
+	{
+		return $this->db->query("SELECT *,SUM(saldo) as total FROM `transaksi` WHERE jenis_saldo='debit' AND YEAR(tgl_transaksi)='" . $tahun . "'")->result();
+	}
+	public function hasilkredit_laba($tahun)
+	{
+		return $this->db->query("SELECT *,SUM(saldo) as total FROM `transaksi` WHERE jenis_saldo='kredit' AND YEAR(tgl_transaksi)='" . $tahun . "'")->result();
+	}
+
+
+	// NERACA
+	public function hasil_lancar($bulan, $tahun)
+	{
+		return $this->db->query("SELECT akun.no_reff,nama_reff,jenis_saldo,saldo,keterangan FROM transaksi LEFT JOIN akun ON transaksi.no_reff=akun.no_reff WHERE akun.no_reff like '1-1%' AND tgl_transaksi BETWEEN '$bulan' AND '$tahun' group by no_reff,nama_reff")->result();
+		// return $this->db->query("SELECT (sum(debet)-sum(kredit)) as hasil,akun.no_reff,nama_reff FROM transaksi LEFT JOIN akun ON transaksi.no_reff=akun.no_reff WHERE akun.no_reff like '1-1%' AND tgl_transaksi BETWEEN '$dari' AND '$sampai' group by no_reff,nama_reff")->result();
+	}
+	public function hasil_tetap($bulan, $tahun)
+	{
+		return $this->db->query("SELECT akun.no_reff,nama_reff,jenis_saldo,saldo,keterangan FROM transaksi LEFT JOIN akun ON transaksi.no_reff=akun.no_reff WHERE akun.no_reff like '1-2%' AND tgl_transaksi BETWEEN '$bulan' AND '$tahun' group by no_reff,nama_reff")->result();
+		// return $this->db->query("SELECT (sum(debet)-sum(kredit)) as hasil,akun.no_reff,nama_reff FROM transaksi LEFT JOIN akun ON transaksi.no_reff=akun.no_reff WHERE akun.no_reff like '1-2%' AND tgl_transaksi BETWEEN '$dari' AND '$sampai' group by no_reff,nama_reff")->result();
+	}
+	public function hasil_pasiva($bulan, $tahun)
+	{
+		return $this->db->query("SELECT akun.no_reff,nama_reff,jenis_saldo,saldo,keterangan FROM transaksi LEFT JOIN akun ON transaksi.no_reff=akun.no_reff WHERE akun.no_reff like '2%' AND tgl_transaksi BETWEEN '$bulan' AND '$tahun' group by no_reff,nama_reff")->result();
+		// return $this->db->query("SELECT (sum(debet)-sum(kredit)) as hasil,akun.no_reff,nama_reff FROM transaksi LEFT JOIN akun ON transaksi.no_reff=akun.no_reff WHERE akun.no_reff like '2%' AND tgl_transaksi BETWEEN '$dari' AND '$sampai' group by no_reff,nama_reff")->result();
+	}
+	public function jurnaldetail_neraca($no_reff, $bulan, $tahun)
+	{
+		return $this->db->query("SELECT transaksi.id_transaksi,transaksi.tgl_transaksi,akun.nama_reff,transaksi.no_reff,transaksi.jenis_saldo,transaksi.saldo,transaksi.tgl_input,MONTHNAME(tgl_transaksi) AS bulan, YEAR(tgl_transaksi) AS tahun FROM `transaksi` LEFT JOIN `akun` ON `akun`.`no_reff` = `transaksi`.`no_reff` WHERE transaksi.no_reff='" . $no_reff . "' AND MONTHNAME(tgl_transaksi)='" . $bulan . "' AND YEAR(tgl_transaksi)='" . $tahun . "'")->result();
+	}
+	public function jurnaldetailsaldo_neraca($no_reff, $bulan, $tahun)
+	{
+		return $this->db->query("SELECT transaksi.jenis_saldo,transaksi.saldo,MONTHNAME(tgl_transaksi) AS bulan, YEAR(tgl_transaksi) AS tahun FROM `transaksi` LEFT JOIN `akun` ON `akun`.`no_reff` = `transaksi`.`no_reff` WHERE transaksi.no_reff='" . $no_reff . "' AND MONTHNAME(tgl_transaksi)='" . $bulan . "' AND YEAR(tgl_transaksi)='" . $tahun . "'")->result();
+	}
 }
 
 /* End of file M_laporan.php */
