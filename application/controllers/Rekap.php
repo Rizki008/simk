86ea08234f2b60385aef7487dae73073715cbb35
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-12-18 15:45:33 +0700

    menambahkan session setiap tampilan

--- application/controllers/Rekap.php
+++ application/controllers/Rekap.php
@@ -19,30 +19,57 @@ class Rekap extends CI_Controller
 
 	public function pokok()
 	{
-		$data = array(
-			'title' => 'Data Rekap Simpanan Pokok',
-			'bulantahun' => $this->m_rekap->bulantahun(),
-			'isi' => 'frontend/rekap/pokok/v_rekapmain'
-		);
-		$this->load->view('frontend/v_wrapper', $data, FALSE);
+		if ($this->session->userdata('level_user') === 1) {
+			$data = array(
+				'title' => 'Data Rekap Simpanan Pokok',
+				'bulantahun' => $this->m_rekap->bulantahun(),
+				'isi' => 'frontend/rekap/pokok/v_rekapmain'
+			);
+			$this->load->view('frontend/v_wrapper', $data, FALSE);
+		} else {
+			$data = array(
+				'title' => 'Data Rekap Simpanan Pokok',
+				'bulantahun' => $this->m_rekap->bulantahun(),
+				'isi' => 'backend/rekap/pokok/v_rekapmain'
+			);
+			$this->load->view('backend/v_wrapper', $data, FALSE);
+		}
 	}
 	public function wajib()
 	{
-		$data = array(
-			'title' => 'Data Rekap Simpanan Wajib',
-			'bulantahun' => $this->m_rekap->bulantahun(),
-			'isi' => 'frontend/rekap/wajib/v_rekapmain'
-		);
-		$this->load->view('frontend/v_wrapper', $data, FALSE);
+		if ($this->session->userdata('level_user') === 1) {
+			$data = array(
+				'title' => 'Data Rekap Simpanan Wajib',
+				'bulantahun' => $this->m_rekap->bulantahun(),
+				'isi' => 'frontend/rekap/wajib/v_rekapmain'
+			);
+			$this->load->view('frontend/v_wrapper', $data, FALSE);
+		} else {
+			$data = array(
+				'title' => 'Data Rekap Simpanan Wajib',
+				'bulantahun' => $this->m_rekap->bulantahun(),
+				'isi' => 'backend/rekap/wajib/v_rekapmain'
+			);
+			$this->load->view('backend/v_wrapper', $data, FALSE);
+		}
 	}
 	public function sukarela()
 	{
-		$data = array(
-			'title' => 'Data Rekap Simpanan Sukarela',
-			'bulantahun' => $this->m_rekap->bulantahun(),
-			'isi' => 'frontend/rekap/sukarela/v_rekapmain'
-		);
-		$this->load->view('frontend/v_wrapper', $data, FALSE);
+		if ($this->session->userdata('level_user') === 1) {
+			$data = array(
+				'title' => 'Data Rekap Simpanan Sukarela',
+				'bulantahun' => $this->m_rekap->bulantahun(),
+				'isi' => 'frontend/rekap/sukarela/v_rekapmain'
+			);
+			$this->load->view('frontend/v_wrapper', $data, FALSE);
+		} else {
+			$data = array(
+				'title' => 'Data Rekap Simpanan Sukarela',
+				'bulantahun' => $this->m_rekap->bulantahun(),
+				'isi' => 'backend/rekap/sukarela/v_rekapmain'
+			);
+			$this->load->view('backend/v_wrapper', $data, FALSE);
+		}
 	}
 
 	public function cetak_pokok()
@@ -53,13 +80,21 @@ class Rekap extends CI_Controller
 
 		$pokok = $this->m_rekap->pokokdetail($bulan, $tahun);
 
-
-		$data = $this->load->view('frontend/rekap/pokok/v_pokok', compact('title', 'bulan', 'tahun', 'pokok'), true);
+		if ($this->session->userdata('level_user') === 1) {
+			$data = $this->load->view('frontend/rekap/pokok/v_pokok', compact('title', 'bulan', 'tahun', 'pokok'), true);
+		} else {
+			$data = $this->load->view('backend/rekap/pokok/v_pokok', compact('title', 'bulan', 'tahun', 'pokok'), true);
+		}
 
 		$this->load->library('pdf');
 		$this->pdf->setPaper('A4', 'landscape');
 		$this->pdf->filename = "rekap_" . $bulan . '_' . $tahun;
-		$this->pdf->load_view('frontend/rekap/pokok/v_pokok', $data);
+
+		if ($this->session->userdata('level_user') === 1) {
+			$this->pdf->load_view('frontend/rekap/pokok/v_pokok', $data);
+		} else {
+			$this->pdf->load_view('backend/rekap/pokok/v_pokok', $data);
+		}
 	}
 	public function cetak_wajib()
 	{
@@ -72,14 +107,23 @@ class Rekap extends CI_Controller
 		$data = null;
 		$saldo = null;
 
-		$data = $this->load->view('frontend/rekap/wajib/v_wajib', compact('title', 'bulan', 'tahun', 'wajib'), true);
-		// echo var_dump($data);
-		// die();
+		if ($this->session->userdata('level_user') === 1) {
+			$data = $this->load->view('frontend/rekap/wajib/v_wajib', compact('title', 'bulan', 'tahun', 'wajib'), true);
+			// echo var_dump($data);
+			// die();
+		} else {
+			$data = $this->load->view('backend/rekap/wajib/v_wajib', compact('title', 'bulan', 'tahun', 'wajib'), true);
+		}
 
 		$this->load->library('pdf');
 		$this->pdf->setPaper('A4', 'landscape');
 		$this->pdf->filename = "rekap_" . $bulan . '_' . $tahun;
-		$this->pdf->load_view('frontend/rekap/wajib/v_wajib', $data);
+
+		if ($this->session->userdata('level_user') === 1) {
+			$this->pdf->load_view('frontend/rekap/wajib/v_wajib', $data);
+		} else {
+			$this->pdf->load_view('backend/rekap/wajib/v_wajib', $data);
+		}
 	}
 	public function cetak_sukarela()
 	{
@@ -89,13 +133,21 @@ class Rekap extends CI_Controller
 
 		$sukarela = $this->m_rekap->sukareladetail($bulan, $tahun);
 
-		$data = $this->load->view('frontend/rekap/sukarela/v_sukarela', compact('title', 'bulan', 'tahun', 'sukarela'), true);
-
+		if ($this->session->userdata('level_user') === 1) {
+			$data = $this->load->view('frontend/rekap/sukarela/v_sukarela', compact('title', 'bulan', 'tahun', 'sukarela'), true);
+		} else {
+			$data = $this->load->view('backend/rekap/sukarela/v_sukarela', compact('title', 'bulan', 'tahun', 'sukarela'), true);
+		}
 
 		$this->load->library('pdf');
 		$this->pdf->setPaper('A4', 'landscape');
 		$this->pdf->filename = "rekap_" . $bulan . '_' . $tahun;
-		$this->pdf->load_view('frontend/rekap/sukarela/v_sukarela', $data);
+
+		if ($this->session->userdata('level_user') === 1) {
+			$this->pdf->load_view('frontend/rekap/sukarela/v_sukarela', $data);
+		} else {
+			$this->pdf->load_view('backend/rekap/sukarela/v_sukarela', $data);
+		}
 	}
 }
 
